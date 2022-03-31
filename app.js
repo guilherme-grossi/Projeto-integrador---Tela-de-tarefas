b956881b2777e1716a299a19f272775af74fba70
guilherme-grossi <guilhermegrossi1000@gmail.com>
2022-03-31 15:59:36 -0300

    testando
    testando

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 let app = express();
 
 
-app.get('/', (req, res)=>res.send("olá mundo!"));
-app.get('/contatos', (req, res)=>res.send({nome: "Guilherme", idade:31}));
+// app.get('/', (req, res)=>res.send("olá mundo!"));
+// app.get('/contatos', (req, res)=>res.send({nome: "Guilherme", idade:31}));
 // criando um servidor
 app.listen(3000, ()=>console.log("Servidor rodando na porta 3000"))
